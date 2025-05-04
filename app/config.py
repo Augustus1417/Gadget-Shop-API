693deadb4362b9595cd159d7d0a68053cf55770b
Augustus1417 <jedxyril.cruz@gmail.com>
2025-05-04 23:56:21 +0800

    jwt settings added

--- app/config.py
+++ app/config.py
@@ -1,14 +1,19 @@
 from pydantic_settings import BaseSettings
 from pathlib import Path
 
-env_path = Path(__file__).resolve().parent.parent / '.env'
+# get the .env file path from outside the app dir
+env_path = Path(__file__).resolve().parent.parent / '.env' 
 
 class Settings(BaseSettings):
     USERNAME: str
     PASSWORD: str
     HOSTNAME: str
     DATABASE: str
-    
+    PASS_HASH: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
     class Config:
         env_file = str(env_path)
 
