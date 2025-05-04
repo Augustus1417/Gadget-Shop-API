64fd0e71f12f9fea5c5e7b685ec9f943f120dfef
Augustus1417 <jedxyril.cruz@gmail.com>
2025-05-04 23:57:53 +0800

    authentication route added

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from .routers.products import router
+from .routers.products import product_router
+from .routers.auth import auth_router
 from . import models
 from .database import engine
+from .config import Settings
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(router,tags=['Products'], prefix='/api/products')
+app.include_router(auth_router,tags=['Authentication'], prefix='/api/auth')
+app.include_router(product_router,tags=['Products'], prefix='/api/products')
