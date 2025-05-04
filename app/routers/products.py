2088521c7c470b2656f9ff856318e9ccb654608f
Augustus1417 <jedxyril.cruz@gmail.com>
2025-05-04 23:57:17 +0800

    changed router name

--- app/routers/products.py
+++ app/routers/products.py
@@ -3,36 +3,36 @@ from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException, status, APIRouter, Response
 from ..database import get_db
 
-router = APIRouter()
+product_router = APIRouter()
 
-@router.get('/', response_model=list[schemas.ProductBaseSchema])
+@product_router.get('/', response_model=list[schemas.ProductBaseSchema])
 def get_products(db: Session=Depends(get_db)):
     products = db.query(models.Products).all()
     return products
 
-@router.get('/phones', response_model=list[schemas.ProductBaseSchema])
+@product_router.get('/phones', response_model=list[schemas.ProductBaseSchema])
 def get_phones(db: Session=Depends(get_db)):
     phones = db.query(models.Products).filter(models.Products.category == "phone").all()
     return phones
 
-@router.get('/laptops', response_model=list[schemas.ProductBaseSchema])
+@product_router.get('/laptops', response_model=list[schemas.ProductBaseSchema])
 def get_laptops(db: Session=Depends(get_db)):
     laptops = db.query(models.Products).filter(models.Products.category == "laptop").all()
     return laptops
 
-@router.get('/accessories', response_model=list[schemas.ProductBaseSchema])
+@product_router.get('/accessories', response_model=list[schemas.ProductBaseSchema])
 def get_accessories(db: Session=Depends(get_db)):
     accessories = db.query(models.Products).filter(models.Products.category == "accessory").all()
     return accessories
 
-@router.get('/get/{product_id}', response_model=schemas.ProductBaseSchema)
+@product_router.get('/get/{product_id}', response_model=schemas.ProductBaseSchema)
 def get_product_by_id(product_id: int, db: Session=Depends(get_db)):
     product = db.query(models.Products).filter(models.Products.product_id == product_id).first()
     if not product:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
     return product
 
-@router.post('/', response_model=schemas.ProductBaseSchema, status_code=status.HTTP_201_CREATED)
+@product_router.post('/', response_model=schemas.ProductBaseSchema, status_code=status.HTTP_201_CREATED)
 def create_product(payload: schemas.NewProduct, db: Session=Depends(get_db)):
     new_product = models.Products(**payload.model_dump())
     db.add(new_product)
@@ -40,7 +40,7 @@ def create_product(payload: schemas.NewProduct, db: Session=Depends(get_db)):
     db.refresh(new_product)
     return new_product
 
-@router.delete('/delete/{product_id}')
+@product_router.delete('/delete/{product_id}')
 def delete_product(product_id: int, db: Session=Depends(get_db)):
     product_query = db.query(models.Products).filter(models.Products.product_id == product_id)
     product = product_query.first()
@@ -50,7 +50,7 @@ def delete_product(product_id: int, db: Session=Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@router.patch('/update/{product_id}')
+@product_router.patch('/update/{product_id}')
 def update_product(product_id: int,payload: schemas.NewProduct, db: Session=Depends(get_db)):
     product_query = db.query(models.Products).filter(models.Products.product_id == product_id)
     product = product_query.first()
