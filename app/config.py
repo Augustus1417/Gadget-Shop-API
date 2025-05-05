91db5b419230f23696e656013fa01cdfac3a201b
Augustus1417 <jedxyril.cruz@gmail.com>
2025-05-06 00:27:49 +0800

    cors origins added

--- app/config.py
+++ app/config.py
@@ -13,6 +13,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    CORS_ORIGINS: str
 
     class Config:
         env_file = str(env_path)
--- app/main.py
+++ app/main.py
@@ -4,10 +4,21 @@ from .routers.auth import auth_router
 from . import models
 from .database import engine
 from .config import Settings
+from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = Settings.CORS_ORIGINS.split(',')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            
+    allow_credentials=True,
+    allow_methods=["*"],              
+    allow_headers=["*"],              
+)
 
 app.include_router(auth_router,tags=['Authentication'], prefix='/api/auth')
 app.include_router(product_router,tags=['Products'], prefix='/api/products')
